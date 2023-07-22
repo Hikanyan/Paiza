0e250d03c9b668858f63678a352e1e51d6d82345
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-22 16:17:49 +0900

    [Update]ListをStackに変更

--- works/mondai/stack_queue/stack_queue__stack_step1/stack_queue__stack_step1/Program.cs
+++ works/mondai/stack_queue/stack_queue__stack_step1/stack_queue__stack_step1/Program.cs
@@ -1,6 +1,8 @@
 ﻿//https://paiza.jp/works/mondai/stack_queue/stack_queue__stack_step1
+
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
@@ -9,28 +11,26 @@ class Program
         // 1行目には、命令の数を表す整数 q が与えられます。
         int q = int.Parse(Console.ReadLine());
         // 続く q 行には、命令を表す文字列 s_i が与えられます。
-        List<int> a = new List<int>();
+        Stack<int> a = new Stack<int>();
 
         //このforでは、命令の数だけ繰り返し処理を行う
         for (int i = 0; i < q; i++)
         {
             //命令を表す文字列 s_i が与えられます。
-            string[] query = Console.ReadLine().Split(' ');
+            string[] query = Console.ReadLine().Split();
             //命令の種類を表す整数 type と、X の値を表す整数 X が与えられます。
             int type = int.Parse(query[0]);
 
             //命令の種類が 1 の場合は、スタックに X を積む命令です。
             if (type == 1)
-            {
-                a.Add(int.Parse(query[1]));
-            }
-            //命令の種類が 2 の場合は、スタックから要素を 1 つ取り出す命令です。
+                a.Push(int.Parse(query[1]));
+            
             else if (type == 2)
-            {
                 if (a.Count > 0)
-                    a.RemoveAt(a.Count - 1);
-            }
-            Console.WriteLine(string.Join(" ", a));
+                    a.Pop();
+
+            //要素の数を反転させて出力します。
+            Console.WriteLine(string.Join(" ", a.Reverse()));
         }
     }
 }
