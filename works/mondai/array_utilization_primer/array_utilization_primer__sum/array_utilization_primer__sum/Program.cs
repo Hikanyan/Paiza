67e23390824f9582ad2f5f55d6ebde6ac26f2aea
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-23 04:20:51 +0900

    [Add]array_utilization_primer__sum

---
+++ works/mondai/array_utilization_primer/array_utilization_primer__sum/array_utilization_primer__sum/Program.cs
@@ -0,0 +1,19 @@
+﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__sum
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int line = int.Parse(Console.ReadLine());
+        int sum = 0;
+        for(int i = 0; i < line; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            sum += n;
+        }
+        Console.WriteLine(sum);
+    }
+}
