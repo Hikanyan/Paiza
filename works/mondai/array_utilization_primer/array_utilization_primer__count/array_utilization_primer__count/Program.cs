37698cbdcae20c8a7a1d9acb8b0ad018aa7039f9
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-23 04:19:49 +0900

    [Add]array_utilization_primer__count

---
+++ works/mondai/array_utilization_primer/array_utilization_primer__count/array_utilization_primer__count/Program.cs
@@ -0,0 +1,18 @@
+﻿//https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__count
+ 
+ using System;
+ 
+ class Program
+ {
+     static void Main()
+     {
+         int[] n = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+         int count = 0;
+         for (int i = 0; i < n[0]; i++)
+         {
+             var a = int.Parse(Console.ReadLine());
+             if (n[1] == a) count++;
+         }
+         Console.WriteLine(count);
+     }
+ }
