93b7d2d65e6d289776f478fcdac5e749c8bde311
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-22 16:19:21 +0900

    [Add]stack_queue__queue_step2

---
+++ works/mondai/stack_queue/stack_queue__queue_step2/stack_queue__queue_step2/Program.cs
@@ -0,0 +1,28 @@
+﻿//https://paiza.jp/works/mondai/stack_queue/stack_queue__queue_step2
+using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        int q = int.Parse(Console.ReadLine());
+        Queue<string> a = new Queue<string>();
+        
+        for (int i = 0; i < q; i++)
+        {
+            string[] query = Console.ReadLine().Split();
+            int queryType = int.Parse(query[0]);
+            
+            if (queryType == 1)
+            {
+                Console.WriteLine();
+                a.Enqueue(query[1]);
+            }
+            else if (queryType == 2)
+            {
+                a.Dequeue();
+            }
+        }
+    }
+}
