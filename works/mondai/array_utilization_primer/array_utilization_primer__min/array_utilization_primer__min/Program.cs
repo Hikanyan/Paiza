5c7d2cace7aa053e3b84854454e1d7fd4e8b3005
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-23 22:20:27 +0900

    [Add]array_utilization_primer__min

---
+++ works/mondai/array_utilization_primer/array_utilization_primer__min/array_utilization_primer__min/Program.cs
@@ -0,0 +1,22 @@
+﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__min
+using System;
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[n];
+        int min = int.MaxValue;
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = int.Parse(Console.ReadLine());
+            if (min > a[i])
+            {
+                min = a[i];
+            }
+        }
+        Console.WriteLine(min);
+    }
+}
