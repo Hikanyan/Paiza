df24ab3d7588cb64bc622ebf395fffd473666b37
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-23 22:19:20 +0900

    [Add]array_utilization_primer__exist

---
+++ works/mondai/array_utilization_primer/array_utilization_primer__exist/array_utilization_primer__exist/Program.cs
@@ -0,0 +1,20 @@
+﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__exist
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] n = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        bool ans = false;
+        for(int i = 0; i < n[0]; i++)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if(a == n[1])
+            {
+                ans = true;
+                break;
+            }
+        }
+        Console.WriteLine(ans ? "Yes" : "No");
+    }
+}
