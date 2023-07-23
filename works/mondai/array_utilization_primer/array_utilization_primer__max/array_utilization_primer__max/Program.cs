3206c2da7359826a6d380d166a6cc717482f3e4a
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-23 22:18:29 +0900

    [Update]array_utilization_primer__max

--- works/mondai/array_utilization_primer/array_utilization_primer__max/array_utilization_primer__max/Program.cs
+++ works/mondai/array_utilization_primer/array_utilization_primer__max/array_utilization_primer__max/Program.cs
@@ -1,3 +1,21 @@
 ﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__max
 
-Console.WriteLine("Hello, World!");
+using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[n];
+        int max = int.MinValue;
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = int.Parse(Console.ReadLine());
+            if (max < a[i])
+            {
+                max = a[i];
+            }
+        }
+        Console.WriteLine(max);
+    }
+}
