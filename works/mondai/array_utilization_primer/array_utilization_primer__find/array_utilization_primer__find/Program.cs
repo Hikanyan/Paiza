43fcb6b24268539fac589d765818522f8224a64b
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-24 20:16:42 +0900

    [Add]array_utilization_primer__find

---
+++ works/mondai/array_utilization_primer/array_utilization_primer__find/array_utilization_primer__find/Program.cs
@@ -0,0 +1,22 @@
+﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__find
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] n = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int count = -1;
+        for (int i = 0; i < n[0]; i++)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a == n[1])
+            {
+                count = i + 1;
+                break;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
