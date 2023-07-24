99f3b86c77592f9e8b45c2d5b94c1250c65ec9bd
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-24 20:42:28 +0900

    [Add]array_utilization_primer__kind

---
+++ works/mondai/array_utilization_primer/array_utilization_primer__kind/array_utilization_primer__kind/Program.cs
@@ -0,0 +1,18 @@
+﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__kind
+using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        //この変数は異なる値を格納するためのコレクションです。
+        HashSet<int> uniqueValues = new HashSet<int>();
+        for(int i = 0; i < n; i++)
+        {
+            int a = int.Parse(Console.ReadLine());
+            uniqueValues.Add(a);
+        }
+        Console.WriteLine(uniqueValues.Count);
+    }
+}
