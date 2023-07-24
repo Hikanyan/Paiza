d2b1923c600876bc5d331c21acc9d516709ac3c4
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-24 20:49:08 +0900

    [Add]array_utilization_primer__map

---
+++ works/mondai/array_utilization_primer/array_utilization_primer__map/array_utilization_primer__map/Program.cs
@@ -0,0 +1,14 @@
+﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__map
+using System;
+class Program
+{
+    static void Main()
+    {
+        int[] n = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        for(int i = 0; i < n[0]; i++)
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a+n[1]);
+        }
+    }
+}
