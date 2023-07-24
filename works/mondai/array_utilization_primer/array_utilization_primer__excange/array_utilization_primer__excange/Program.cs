fd4e0a1ffca48974741d4a44c2b85c5ce34f18ca
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-24 21:11:16 +0900

    [Add]array_utilization_primer__excange

---
+++ works/mondai/array_utilization_primer/array_utilization_primer__excange/array_utilization_primer__excange/Program.cs
@@ -0,0 +1,13 @@
+﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__excange
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        Console.WriteLine($"{array[1]} {array[0]}");
+    }
+}
