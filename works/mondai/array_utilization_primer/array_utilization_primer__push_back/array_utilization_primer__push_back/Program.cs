2e423f8facbb2274a4f7175b8b7043b1a911623b
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-25 03:27:52 +0900

    [Add]array_utilization_primer__push_back

---
+++ works/mondai/array_utilization_primer/array_utilization_primer__push_back/array_utilization_primer__push_back/Program.cs
@@ -0,0 +1,15 @@
+﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__push_back
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i <= n; i++)
+        {
+            Console.WriteLine(int.Parse(Console.ReadLine()));
+        }
+    }
+}
