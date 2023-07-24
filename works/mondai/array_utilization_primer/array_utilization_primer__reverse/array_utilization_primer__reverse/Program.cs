7a1fce87640eabdf325314278eec660a1b82e92d
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-24 20:53:23 +0900

    [Add]array_utilization_primer__reverse

---
+++ works/mondai/array_utilization_primer/array_utilization_primer__reverse/array_utilization_primer__reverse/Program.cs
@@ -0,0 +1,17 @@
+﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__reverse
+using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = int.Parse(Console.ReadLine());
+            b[n - i - 1] = a[i];
+        }
+        Console.WriteLine(string.Join("\n", b));
+    }
+}
