2b1d687e7e59f80e88aa19adf275968cf7009227
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-29 08:19:02 +0900

    [Add]sort_naive_insesrtion

---
+++ works/mondai/sort_naive/sort_naive__insertion/sort_naive__insertion/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class InsertSort
+{
+    static void Main(string[] args)
+    {
+        Console.ReadLine(); // 最初の行は不要なので読み飛ばす
+        var array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        List<string> ans = new List<string>();
+
+        insertSort(array, ans);
+
+        Console.WriteLine(string.Join("\n", ans)); // 各ステップの結果を改行で区切って出力
+    }
+
+    // 挿入ソート
+    private static void insertSort(int[] array, List<string> ans) 
+    {
+        // ここに、挿入ソートを実装して、途中経過を ans に追加する
+        for (int i = 0; i < array.Length; i++)
+        {
+            int temp = array[i]; //配列のi番目の値をtempに代入
+            int j = i - 1; //配列のi番目の値の一つ前の値をjに代入
+            while (j >= 0 && array[j] > temp)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = temp;
+
+            // 挿入ソートの各ステップごとの途中結果を文字列としてansに追加
+            //最初のArrayは出力しない
+            if (i == 0) continue;
+            ans.Add(string.Join(" ", array));
+        }
+    }
+}
---
+++ works/mondai/sort_naive/sort_naive__selection/sort_naive__selection/Program.cs
@@ -0,0 +1,45 @@
+﻿//https://paiza.jp/works/mondai/sort_naive/sort_naive__selection
+using System;
+using System.Collections.Generic;
+
+public class SelectionSort
+{
+    static void Main(string[] args)
+    {
+        Console.ReadLine(); // 最初の行は不要なので読み飛ばす
+        var array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        List<string> ans = new List<string>();
+
+        SelectionSortArray(array, ans);
+
+        Console.WriteLine(string.Join("\n", ans)); // 各ステップの結果を改行で区切って出力
+    }
+
+    // 選択ソート
+    private static void SelectionSortArray(int[] array, List<string> ans)
+    {
+        //array.Length - 1 は最後の要素のインデックス
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            int minIndex = Int32.MaxValue; // 最小値のインデックスを保持
+            //i + 1 は最初の要素のインデックス
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j] < array[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            // 最小値を先頭に移動(Paizaではできない)
+            //(array[i], array[minIndex]) = (array[minIndex], array[i]);
+
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+
+            // 選択ソートの各ステップごとの途中結果を文字列としてansに追加
+            ans.Add(string.Join(" ", array));
+        }
+    }
+}
