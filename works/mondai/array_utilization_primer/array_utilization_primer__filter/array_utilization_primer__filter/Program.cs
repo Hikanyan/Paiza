2c0e0d9a5cd78be25ebdacebf78e363881f87da3
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-29 20:36:25 +0900

    [Add]filter

---
+++ works/mondai/array_utilization_primer/array_utilization_primer__filter/array_utilization_primer__filter/Program.cs
@@ -0,0 +1,20 @@
+﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__filter
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] n = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        
+        for(int i = 0; i < n[0]; i++)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (n[1] <= a)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
