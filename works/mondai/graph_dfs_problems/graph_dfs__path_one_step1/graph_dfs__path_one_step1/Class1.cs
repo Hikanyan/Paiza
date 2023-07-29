55944c16b822cfe0ce6539f61fcdce56e64c9859
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-29 17:22:05 +0900

    [Add]graph_dfs__path_one_step1

---
+++ works/mondai/graph_dfs_problems/graph_dfs__path_one_step1/graph_dfs__path_one_step1/Class1.cs
@@ -0,0 +1,22 @@
+﻿//https://paiza.jp/works/mondai/graph_dfs_problems/graph_dfs__path_one_step1
+using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        var date = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        var n = date[0];//n 個の頂点
+        var s = date[1];//頂点 s に隣接している頂点
+        List<int[]> adjacencyList = new List<int[]>();
+
+        //隣接リストの作成
+        for (int i = 0; i < n; i++)
+        {
+            Console.ReadLine();//次に来る頂点個数なのだがC#においては不要
+            date = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            adjacencyList.Add(date);
+        }
+        Console.WriteLine(adjacencyList[s-1].Max());
+    }
+}
