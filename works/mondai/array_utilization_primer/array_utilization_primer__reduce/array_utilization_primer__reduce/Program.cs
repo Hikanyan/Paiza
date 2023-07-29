7f09e29be934fa670f5a47614439d8380fdb07a9
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-29 20:18:40 +0900

    [Add]reduce

---
+++ works/mondai/array_utilization_primer/array_utilization_primer__reduce/array_utilization_primer__reduce/Program.cs
@@ -0,0 +1,25 @@
+﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__reduce
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = int.Parse(Console.ReadLine());
+        }
+        //二重ループで全ての組み合わせを出力
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 0; j < i ; j++)
+            {
+                int result = a[i] * a[j];
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
