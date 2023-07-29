9917949c9f0c311e793ae5e85e2fc7742fc554d4
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-29 20:19:15 +0900

    [Add]easy

---
+++ works/mondai/array_utilization_primer/array_utilization_primer__reduse_easy/array_utilization_primer__reduse_easy/Program.cs
@@ -0,0 +1,17 @@
+﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__reduse_easy
+using System;
+class Program
+{
+    static void Main()
+    {
+        
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = 1; j <= 9; j++)
+            {
+                Console.Write($"{i * j}{(j == 9 ? "" : " ")}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
