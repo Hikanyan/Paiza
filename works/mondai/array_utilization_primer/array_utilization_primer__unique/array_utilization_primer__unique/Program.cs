370c4d7f41338ab4dba793c6bd6bb08a129f2a46
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-29 20:36:06 +0900

    [Update]unique

--- works/mondai/array_utilization_primer/array_utilization_primer__unique/array_utilization_primer__unique/Program.cs
+++ works/mondai/array_utilization_primer/array_utilization_primer__unique/array_utilization_primer__unique/Program.cs
@@ -1,3 +1,26 @@
 ﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__unique
 
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var a = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = int.Parse(Console.ReadLine());
+        }
+        List<int> b = new List<int>();
+        foreach (var num in a)
+        {
+            if(!b.Contains(num))
+            {
+                b.Add(num);
+            }
+        }
+        Console.WriteLine(string.Join("\n", b));
+    }
+}
