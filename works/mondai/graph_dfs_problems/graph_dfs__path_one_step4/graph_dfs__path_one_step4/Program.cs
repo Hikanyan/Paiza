c9cc2b120095737444097d029ec7a3be72ac45f6
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-29 20:17:57 +0900

    [Add]graph_dfs__path_one_step4

---
+++ works/mondai/graph_dfs_problems/graph_dfs__path_one_step4/graph_dfs__path_one_step4/Program.cs
@@ -0,0 +1,12 @@
+﻿// https://paiza.jp/works/mondai/graph_dfs_problems/graph_dfs__path_one_step4
+using System;
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var line = Console.ReadLine();
+        Console.WriteLine("XXXXXX");
+    }
+}
