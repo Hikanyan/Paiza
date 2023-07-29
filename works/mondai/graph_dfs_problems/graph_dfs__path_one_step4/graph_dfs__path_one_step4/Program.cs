e9039636dc0f92f63c341a2024c6507fbb639d38
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-30 01:02:57 +0900

    [Update]

--- works/mondai/graph_dfs_problems/graph_dfs__path_one_step4/graph_dfs__path_one_step4/Program.cs
+++ works/mondai/graph_dfs_problems/graph_dfs__path_one_step4/graph_dfs__path_one_step4/Program.cs
@@ -1,12 +1,64 @@
 ﻿// https://paiza.jp/works/mondai/graph_dfs_problems/graph_dfs__path_one_step4
 using System;
+using System.Collections.Generic;
+
 class Program
 {
-    static void Main()
+    static int n, s, t;
+    static Dictionary<int, List<int>> adList;
+    static List<int> minPath;
+
+    static void Main(string[] args)
     {
-        // 自分の得意な言語で
-        // Let's チャレンジ！！
-        var line = Console.ReadLine();
-        Console.WriteLine("XXXXXX");
+        int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        n = input[0];
+        s = input[1];
+        t = input[2];
+
+        adList = new Dictionary<int, List<int>>();
+        for (int i = 1; i <= n; i++)
+        {
+            Console.ReadLine();
+            adList[i] = new List<int>();
+            int[] adInput = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            foreach (int ad in adInput)
+            {
+                adList[i].Add(ad);
+            }
+        }
+
+        minPath = new List<int>();
+        for (int i = 0; i < n + 1; i++)
+        {
+            minPath.Add(i + 1);
+        }
+
+        DFS(s, new List<int> { s });
+
+        Console.WriteLine(string.Join(" ", minPath));
+    }
+
+    static void DFS(int v, List<int> path)
+    {
+        if (path.Count < minPath.Count)
+        {
+            foreach (int i in adList[v])
+            {
+                if (!path.Contains(i))
+                {
+                    path.Add(i);
+                    if (i == t)
+                    {
+                        minPath = new List<int>(path);
+                    }
+                    else
+                    {
+                        DFS(i, path);
+                    }
+                    path.RemoveAt(path.Count - 1);
+                }
+            }
+        }
     }
 }
+
