51355350435ddb984cea58b16772dc24d182a733
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-07-29 19:08:14 +0900

    [Fix]修正

--- works/mondai/array_utilization_primer/array_utilization_primer__insert/array_utilization_primer__insert/Program.cs
+++ works/mondai/array_utilization_primer/array_utilization_primer__insert/array_utilization_primer__insert/Program.cs
@@ -1,43 +1,29 @@
 ﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__insert
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
         int n = int.Parse(Console.ReadLine());
-        int[] array = new int[n];
+        List<int> array = new List<int>();
+        //arrayに値を追加
         for (int i = 0; i < n; i++)
         {
-            array[i] = int.Parse(Console.ReadLine());
+            array.Add(int.Parse(Console.ReadLine()));
         }
 
+        //追加したい値を配列bに格納
         int[] b = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
 
-        for (int i = 0; i < array.Length; i++)
-        {
-            if (array[i] == b[0])
-            {
-                InsertElement(array, i + 1, b[1]);
-                break;
-            }
-        }
-
+        //arrayのb[0]番目の要素の後ろにb[1]を追加
+        array.Insert(b[0], b[1]);
         foreach (int i in array)
         {
             Console.WriteLine(i);
         }
-    }
-
-    static void InsertElement(int[] array, int index, int element)
-    {
-        Array.Resize(ref array, array.Length + 1);
-        for (int i = array.Length - 1; i > index; i--)
-        {
-            array[i] = array[i - 1];
-        }
-
-        array[index] = element;
     }
 }
