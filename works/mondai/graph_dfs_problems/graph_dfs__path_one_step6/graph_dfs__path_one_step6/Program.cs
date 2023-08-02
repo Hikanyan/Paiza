3897d70258c8fbee1a30d09cf0617dec0e9ab0d2
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-08-02 21:51:05 +0900

    [Update]コメントの追加

--- works/mondai/graph_dfs_problems/graph_dfs__path_one_step6/graph_dfs__path_one_step6/Program.cs
+++ works/mondai/graph_dfs_problems/graph_dfs__path_one_step6/graph_dfs__path_one_step6/Program.cs
@@ -11,27 +11,30 @@ class MainClass
         int n = inputs[0];
         int s = inputs[1];
         int t = inputs[2];
-        Console.ReadLine();
+        Console.ReadLine();//いらない
 
+        //hashSetには、削除する頂点の番号が入る
         HashSet<int> hashSet = new HashSet<int>(Console.ReadLine().Split().Select(int.Parse));
-
+        //adListには、隣接リストが入る
         List<List<int>> adList = new List<List<int>>();
+        //隣接リストの初期化
         for (int i = 0; i <= n; i++)
         {
             adList.Add(new List<int>());
         }
 
+        //隣接リストを作成
         for (int i = 1; i <= n; i++)
         {
             Console.ReadLine();
             adList[i] = Console.ReadLine().Split().Select(int.Parse).ToList();
         }
-
+        //削除する頂点を隣接リストから削除
         for (int i = 1; i <= n; i++)
         {
-            if (hashSet.Contains(i))
+            if (hashSet.Contains(i))//削除する頂点の場合
             {
-                foreach (int j in adList[i].ToList())
+                foreach (int j in adList[i].ToList())//隣接リストから削除
                 {
                     adList[j].Remove(i);
                 }
@@ -39,9 +42,11 @@ class MainClass
             }
         }
 
+        //最短経路を探索
         List<int> minPath = Enumerable.Range(1, n + 1).ToList();
         Dfs(s, new List<int> { s }, adList, t, ref minPath);
 
+        //結果を出力
         if (minPath.Count == n + 1)
         {
             Console.WriteLine(-1);
@@ -52,14 +57,19 @@ class MainClass
         }
     }
 
+    //深さ優先探索
     static void Dfs(int v, List<int> path, List<List<int>> adList, int target, ref List<int> minPath)
     {
+        //終了条件
         if (path.Count < minPath.Count)
         {
+            //隣接リストを探索
             foreach (int i in adList[v])
             {
+                //探索済みでない場合
                 if (!path.Contains(i))
                 {
+                    //探索済みにする
                     path.Add(i);
                     if (i == target)
                     {
@@ -67,8 +77,10 @@ class MainClass
                     }
                     else
                     {
+                        //再帰呼び出し
                         Dfs(i, path, adList, target, ref minPath);
                     }
+                    //探索済みを解除
                     path.RemoveAt(path.Count - 1);
                 }
             }
