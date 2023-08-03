9722872fbead0e1d126560bf90caaa4ef6f06645
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-08-03 23:13:30 +0900

    [Add]exam

---
+++ works/mondai/array_utilization_primer/array_utilization_primer__exam/array_utilization_primer__exam/Program.cs
@@ -0,0 +1,18 @@
+﻿// https://paiza.jp/works/mondai/array_utilization_primer/array_utilization_primer__exam
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] m = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int[][] scores = new int[n][];
+        int max = 0;
+        
+        for (int i = 0; i < n; i++)
+        {
+            scores[i] = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        }
+    }
+}
