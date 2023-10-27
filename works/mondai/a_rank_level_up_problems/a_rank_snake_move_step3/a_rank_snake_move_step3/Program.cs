379bfce309b9ea0585f0d3e84adf5daecdc18d9a
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-10-27 13:14:31 +0900

    [Add]step3

---
+++ works/mondai/a_rank_level_up_problems/a_rank_snake_move_step3/a_rank_snake_move_step3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // 開始座標と向きを取得
+        string[] initialInputs = Console.ReadLine().Split(' ');
+        int startY = int.Parse(initialInputs[0]);
+        int startX = int.Parse(initialInputs[1]);
+        string initialDirection = initialInputs[2];
+
+        // 移動指示を取得
+        string moveDirection = Console.ReadLine();
+
+        // 現在の座標を初期座標に設定
+        int currentY = startY;
+        int currentX = startX;
+
+        if (initialDirection == "N")
+        {
+            if (moveDirection == "L")
+                currentX -= 1;
+            else
+                currentX += 1;
+        }
+        else if (initialDirection == "S")
+        {
+            if (moveDirection == "L")
+                currentX += 1;
+            else
+                currentX -= 1;
+        }
+        else if (initialDirection == "E")
+        {
+            if (moveDirection == "L")
+                currentY -= 1;
+            else
+                currentY += 1;
+        }
+        else if (initialDirection == "W")
+        {
+            if (moveDirection == "L")
+                currentY += 1;
+            else
+                currentY -= 1;
+        }
+
+        Console.WriteLine($"{currentY} {currentX}");
+    }
+}
