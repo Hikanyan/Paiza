60cf02abf4bf7036c8c2fe19c8c686741a21e4cd
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-10-27 13:13:44 +0900

    [Add]step1

---
+++ works/mondai/a_rank_level_up_problems/a_rank_snake_move_step1/a_rank_snake_move_step1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FindHashPosition
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 入力を受け取る
+            string[] inputs = Console.ReadLine().Split(' ');
+            int h = int.Parse(inputs[0]);
+            int w = int.Parse(inputs[1]);
+
+            // '#' の位置を見つけて出力
+            for (int y = 0; y < h; y++)
+            {
+                string line = Console.ReadLine();
+                for (int x = 0; x < w; x++)
+                {
+                    if (line[x] == '#')
+                    {
+                        Console.WriteLine($"{y} {x}");
+                        return;  // '#' は1つだけなので、見つけたら終了
+                    }
+                }
+            }
+        }
+    }
+}
