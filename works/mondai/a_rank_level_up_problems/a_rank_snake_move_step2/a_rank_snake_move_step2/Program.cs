43a667d91d009364676d780f9d5b7a13b69fb086
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-10-27 13:14:15 +0900

    [Add]step2

---
+++ works/mondai/a_rank_level_up_problems/a_rank_snake_move_step2/a_rank_snake_move_step2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FindHashPosition
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 入力を受け取る
+            string[] inputs = Console.ReadLine().Split(' ');
+            int y = int.Parse(inputs[0]);
+            int x = int.Parse(inputs[1]);
+            int n = int.Parse(inputs[2]);
+
+            //各移動方向に従って、移動後の座標を出力する
+            for (int i = 0; i < n; i++)
+            {
+                char direction = Console.ReadLine()[0];
+
+                // 移動後の座標を出力
+                switch (direction)
+                {
+                    case 'N':
+                        y -= 1;
+                        break;
+                    case 'S':
+                        y += 1;
+                        break;
+                    case 'E':
+                        x += 1;
+                        break;
+                    case 'W':
+                        x -= 1;
+                        break;
+                }
+
+                Console.WriteLine($"{y} {x}");
+            }
+        }
+    }
+}
