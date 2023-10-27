ecb0215d3987ae47ebd4930898e1a9a3e7669850
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-10-27 13:13:26 +0900

    [Add]Boss

---
+++ works/mondai/a_rank_level_up_problems/a_rank_snake_move_boss/a_rank_snake_move_boss/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Program
+{
+    static int moveX, moveY;
+    static int currentDirectionIndex = 0; // 北を向いて開始
+    static char[] directions = { 'N', 'E', 'S', 'W' };
+
+    public static void MoveCharacter(char moveDirection)
+    {
+        if (moveDirection == 'L')
+        {
+            currentDirectionIndex = (currentDirectionIndex + 3) % 4; // 左に90度回転
+        }
+        else
+        {
+            currentDirectionIndex = (currentDirectionIndex + 1) % 4; // 右に90度回転
+        }
+
+        switch (directions[currentDirectionIndex])
+        {
+            case 'N': moveY--; break;
+            case 'S': moveY++; break;
+            case 'E': moveX++; break;
+            case 'W': moveX--; break;
+        }
+    }
+
+    public static void Main()
+    {
+        var input = Console.ReadLine().Split(' ');
+        moveX = int.Parse(input[0]);
+        moveY = int.Parse(input[1]);
+        int totalSteps = int.Parse(input[2]);
+
+        for (int i = 0; i < totalSteps; i++)
+        {
+            char moveDirection = Console.ReadLine()[0];
+            MoveCharacter(moveDirection);
+            Console.WriteLine($"{moveX} {moveY}");
+        }
+    }
+}
