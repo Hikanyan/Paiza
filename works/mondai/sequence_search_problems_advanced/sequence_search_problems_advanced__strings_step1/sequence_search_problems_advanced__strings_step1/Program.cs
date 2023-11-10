1291f232ca4c4b69f8c9a17203ced976d5832c4f
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-11-10 15:19:15 +0900

    [Add]

--- works/mondai/sequence_search_problems_advanced/sequence_search_problems_advanced__strings_step1/sequence_search_problems_advanced__strings_step1/Program.cs
+++ works/mondai/sequence_search_problems_advanced/sequence_search_problems_advanced__strings_step1/sequence_search_problems_advanced__strings_step1/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Linq;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        
+
+        //a-zまでの文字を順番に調べる
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            Console.WriteLine(input.Count(x => x == c)); //文字の出現回数を出力
+            
+            // var count = 0; //文字の出現回数
+            // foreach (var ch in input) //文字列の各文字について
+            // {
+            //     if (ch == c) //文字が一致したら
+            //     {
+            //         count++; //カウントを増やす
+            //     }
+            // }
+            //
+            // //出現回数を出力
+            // Console.WriteLine(count);
+        }
+    }
+}
