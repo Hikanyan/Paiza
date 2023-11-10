174ac0f89077bf753d6c48d53063f833a04d82d8
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-11-10 15:20:49 +0900

    [Add]

---
+++ works/mondai/sequence_search_problems_advanced/sequence_search_problems_advanced__strings_step4/sequence_search_problems_advanced__strings_step4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var input = Console.ReadLine().Split(' ');
+        var n = int.Parse(input[0]);
+        var x = int.Parse(input[1]);
+
+        for (var i = 0; i < n; i++)
+        {
+            var line = Console.ReadLine().Split(',');
+            var a = int.Parse(line[0]);
+            var b = int.Parse(line[1]);
+            if (b >= x)
+            {
+                Console.WriteLine($"{a},{b}");
+            }
+        }
+    }
+}
