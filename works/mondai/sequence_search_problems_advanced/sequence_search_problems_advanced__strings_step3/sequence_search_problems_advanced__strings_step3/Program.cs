f038eee2552d897ab4bf19bb2e8117564d6789f2
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-11-10 15:20:37 +0900

    [Add]

---
+++ works/mondai/sequence_search_problems_advanced/sequence_search_problems_advanced__strings_step3/sequence_search_problems_advanced__strings_step3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //
+        // var inputS = Console.ReadLine();
+        // var s = inputS.Split(',');
+        //
+        // //スペースを出力したいから、Joinを使う
+        //
+        // Console.WriteLine(string.Join(" ", s));
+
+
+        //Replaceで置き換える
+        Console.WriteLine(Console.ReadLine().Replace(",", " "));
+    }
+}
