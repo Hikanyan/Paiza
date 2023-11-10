b6871c8f0af24a688bc6fbdf598f384bee42a7de
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-11-10 16:19:33 +0900

    [Add]

---
+++ works/mondai/a_rank_level_up_problems/a_rank_camp_step2/a_rank_camp_step2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var line = Console.ReadLine().Split(' ');
+        var h = int.Parse(line[0]);
+        var w = int.Parse(line[1]);
+        var s = new string[h];
+        for (int i = 0; i < h; i++)
+        {
+            s[i] = Console.ReadLine();
+        }
+
+        var t = new char[h, w];
+
+        // 入力された盤面をコピー
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                t[i, j] = s[i][j];
+            }
+        }
+
+        // '*' から隣接する '.' を '*' に変更
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                if (s[i][j] == '*')
+                {
+                    if (i > 0 && s[i - 1][j] == '.') t[i - 1, j] = '*';
+                    if (i < h - 1 && s[i + 1][j] == '.') t[i + 1, j] = '*';
+                    if (j > 0 && s[i][j - 1] == '.') t[i, j - 1] = '*';
+                    if (j < w - 1 && s[i][j + 1] == '.') t[i, j + 1] = '*';
+                }
+            }
+        }
+
+        // 結果の盤面を出力
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                Console.Write(t[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
