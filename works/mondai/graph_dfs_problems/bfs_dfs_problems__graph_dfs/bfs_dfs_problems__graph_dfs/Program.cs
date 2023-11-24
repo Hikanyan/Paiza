72b9d838f65abc33e6e52092698c7010f96395f0
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-11-24 10:23:57 +0900

    [Add]

---
+++ works/mondai/graph_dfs_problems/bfs_dfs_problems__graph_dfs/bfs_dfs_problems__graph_dfs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var input = Console.ReadLine().Split(' ');
+        var n = int.Parse(input[0]);
+        var m = int.Parse(input[1]);
+        var x = int.Parse(input[2]);
+        var graph = new int[n, n];
+
+        for (int i = 0; i < m; i++)
+        {
+            var input2 = Console.ReadLine().Split(' ');
+            var a = int.Parse(input2[0]);
+            var b = int.Parse(input2[1]);
+            graph[a - 1, b - 1] = 1;
+            graph[b - 1, a - 1] = 1;
+        }
+
+        var visited = new bool[n];
+        visited[x - 1] = true;
+        Console.WriteLine(x);
+        Dfs(graph, visited, x - 1);
+    }
+
+    static void Dfs(int[,] graph, bool[] visited, int x)
+    {
+        for (int i = 0; i < graph.GetLength(0); i++)
+        {
+            if (graph[x, i] == 1 && !visited[i])
+            {
+                visited[i] = true;
+                Console.WriteLine(i + 1);
+                Dfs(graph, visited, i);
+            }
+        }
+    }
+}
